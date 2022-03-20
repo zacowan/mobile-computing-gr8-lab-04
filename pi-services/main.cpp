3a87843ea5b8a4ea416b5da1dad4990db7972655
Zachary Cowan <44091329+zacowan@users.noreply.github.com>
2022-03-20 14:42:44 -0400

    feat(main): added outline for main code

--- pi-services/main.cpp
+++ pi-services/main.cpp
@@ -1,1 +1,93 @@
-// Main
+#include <iostream>
+#include <wiringPi.h>
+#include "mode.cpp"
+
+using namespace std;
+
+/**
+ * START configuration constants
+ * -----------------------------------------------------------------------------
+ */
+
+// Determines the "clock" of the pi
+#define LOOP_DELAY 100
+
+// Define all of the pin numbers below
+#define A_PIN 27
+
+/**
+ * END configuration constants
+ * -----------------------------------------------------------------------------
+ */
+
+/**
+ * START state variables
+ * -----------------------------------------------------------------------------
+ */
+
+// The current mode of the device
+Mode currMode = Mode::AUTO;
+
+/**
+ * END state variables
+ * -----------------------------------------------------------------------------
+ */
+
+/**
+ * @brief Sets up raspberry pi board, pins, and interrupt handlers
+ *
+ */
+void setup()
+{
+    // TODO: setup input pins for monitoring sensor data and voltage on RGB LED
+    // TODO: setup interrupt handler for button
+}
+
+/**
+ * @brief Defines what the pi should do every "clock" tick
+ *
+ */
+void loop()
+{
+    switch (currMode)
+    {
+    case Mode::OFF:
+        // TODO: turn the standard LED is off
+        // TODO: turn the RGB LED to OFF_COLOR
+        break;
+
+    case Mode::AUTO:
+        // TODO: turn standard LED on/off based on sensor data
+        // TODO: turn the RGB LED to AUTO_COLOR
+        break;
+
+    case Mode::ON:
+        // TODO: turn the standard LED is on
+        // TODO: turn the RGB LED to ON_COLOR
+        break;
+
+    default:
+        cerr << "Error: default case" << endl;
+        break;
+    }
+}
+
+int main()
+{
+    // Setup wiringPi
+    if (wiringPiSetup() < 0)
+    {
+        cout << "Failed to setup wiringPi" << endl;
+        return 1;
+    }
+    // Setup board
+    setup();
+    // Start the program
+    while (true)
+    {
+        loop();
+        delay(LOOP_DELAY);
+    }
+
+    return 0;
+}
---
+++ pi-services/mode.cpp
@@ -0,0 +1,8 @@
+#pragma once
+
+enum Mode
+{
+    OFF,
+    AUTO,
+    ON
+};
