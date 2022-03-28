1d9745636f6ccfc15f0816ce3e008d8105a84d14
Shaun Fidler <shaunfidler@gmail.com>
2022-03-28 18:01:18 -0400

    Add functional Atlas portion and service code

--- testing-program/test.py
+++ testing-program/test.py
@@ -2,10 +2,10 @@ from socket import socket, AF_INET, SOCK_STREAM
 import json
 
 # ########## CONSTANTS ##########
-HOST_IP = "192.168.1.1"
+HOST_IP = "192.168.0.111"
 PORT = 6668
-THING_ID = "A"
-SPACE_ID = "B"
+THING_ID = "StrawberryThing01"
+SPACE_ID = "StrawberrySmartSpace"
 # ###############################
 
 
@@ -33,39 +33,36 @@ class Tester():
         self.s = setup_connection()
 
     def get_led(self):
-        tweet = construct_tweet("Get LED")
-        self.s.sendall(tweet)
-        # Not sure if below works
-        received = ""
-        self.s.recv_into(received)
-        print(received)
+        tweet = construct_tweet("get_led")
+        self.s.sendall(str.encode(tweet))
+        data, addr = self.s.recvfrom(1024) # buffer size is 1024 bytes
+        print(data)
 
     def get_light_level(self):
-        tweet = construct_tweet("Get Light Level")
-        self.s.sendall(tweet)
-        # Not sure if below works
-        received = ""
-        self.s.recv_into(received)
-        print(received)
+        tweet = construct_tweet("get_light_level")
+        self.s.sendall(str.encode(tweet))
+        data, addr = self.s.recvfrom(1024) # buffer size is 1024 bytes
+        print(data)
 
     def get_mode(self):
-        tweet = construct_tweet("Get Mode")
-        self.s.sendall(tweet)
-        # Not sure if below works
-        received = ""
-        self.s.recv_into(received)
-        print(received)
+        tweet = construct_tweet("get_mode")
+        self.s.sendall(str.encode(tweet))
+        data, addr = self.s.recvfrom(1024) # buffer size is 1024 bytes
+        print(data)
 
     def toggle_mode(self, new_mode: str):
-        tweet = construct_tweet("Toggle Mode", "({})".format(new_mode))
-        self.s.sendall(tweet)
+        tweet = construct_tweet("toggle_mode", "({})".format(new_mode))
+        self.s.sendall(str.encode(tweet))
         print("Attempted to toggle mode to {}".format(new_mode))
 
 
 def main():
     t = Tester()
 
-    t.get_led()
+    #t.get_led()
+    #t.get_light_level()
+    #t.get_mode()
+    t.toggle_mode(1)
 
 
 if __name__ == "__main__":
